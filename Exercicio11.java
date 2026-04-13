fb956e500d0fc1e5b80064aa5b85478f5fa21702
LorenzoHC <lorenzohcandela@gmail.com>
2026-04-13 19:43:42 -0300

    Update Exercicio11.java

--- Exercicio11.java
+++ Exercicio11.java
@@ -10,6 +10,39 @@ package ado4;
  */
 public class Exercicio11 {
     public static void main(String[] args) {
-        
+                Scanner sc = new Scanner(System.in);
+        int[] v1 = new int[10];
+        int[] v2 = new int[10];
+        int[] Soma = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+
+            System.out.println("Digite 10 numeros para o vetor 1: ");
+            v1[i] = sc.nextInt();
+
+        }
+
+        for (int i = 0; i < 10; i++) {
+
+            System.out.println("Digite 10 numeros para o vetor 2: ");
+            v2[i] = sc.nextInt();
+
+        }
+
+        for (int i = 0; i < 10; i++) {
+
+            Soma[i] = v1[i] + v2[i];
+
+        }
+
+        System.out.println("Resultado:");
+
+        for (int i = 0; i < 10; i++) {
+
+            System.out.println(Soma[i]);
+
+        }
     }
 }
+    
+
